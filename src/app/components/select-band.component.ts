70f43240867f753d3f846df031995fa7e1ec4545
karloscode <carlosjaviergonzalezolmo@gmail.com>
2016-12-28 19:13:01 +0100

    visual enhacements

--- src/app/components/select-band.component.ts
+++ src/app/components/select-band.component.ts
@@ -3,11 +3,19 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'selectband',  
   template: `
-             <article> 
-                <h2>Wellcome to School of Rock</h2> 
-                    <p>Please, Select Band from top menu</p>
-              
-             </article>`
+            <article> 
+                <h1>Wellcome to School of Rock</h1> 
+                <h2>Please, Select Band from top Menu</h2> 
+            </article>
+            <br>
+            <br>
+            <br>
+            <div class="col-sm-12">
+            	<h3>WE</h3>
+     			<span class="glyphicon glyphicon-heart logo-small"></span>
+     			<span class="glyphicon glyphicon-music logo-small"></span>
+      			<h4>LOVE MUSIC</h4>
+    		</div>`
 
   })
 
